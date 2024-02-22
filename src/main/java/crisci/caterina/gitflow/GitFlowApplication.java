ae107c1e93ce2ffd96488f3a8746538de8038055
Katiacrisci <katcrisci@gmail.com>
2024-02-22 15:57:04 +0100

    First commit

---
+++ src/main/java/crisci/caterina/gitflow/GitFlowApplication.java
@@ -0,0 +1,13 @@
+package crisci.caterina.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowApplication.class, args);
+    }
+
+}
